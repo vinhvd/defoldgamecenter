8db2e85146d78c81a5cb69944e88012509661b0f
Vinh Vo <vinhvd@gmail.com>
2017-07-22 21:08:09 +0700

    Refactor GameKitHelper to GameKitManager

--- gamecenter/src/gamecenter.cpp
+++ gamecenter/src/gamecenter.cpp
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+ 
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,17 +34,64 @@
 
 #include "gamecenter_private.h"
 
-
-static int authenticate(lua_State* L)
+/** Authenticate local player, show Game Center login modal if not logged yet. 
+*/
+static int login(lua_State* L)
 {
     DM_LUA_STACK_CHECK(L, 0);
-    authenticate();
-	return 0;
+    login();
+    return 0;
+}
+
+/** Submit a score for a specified Leader Board
+* @param 1 The leaderboard identity to report
+* @param 2 The score to report
+*/
+static int reportScore(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 0);
+    const char *leaderboardId = lua_tostring(L, 1);
+    int score = luaL_checkint(L, 2);
+    int status = reportScore(leaderboardId, score);
+    return 0;
+}
+
+/** Load a specified Leader Board
+* @param 1 The leaderboard identity to load
+*/
+static int showLeaderboard(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 0);
+    const char *leaderboardId = lua_tostring(L, 1);
+    showLeaderboard(leaderboardId);
+    return 0;
+}
+
+/** Load Leader Boards
+*/
+static int showLeaderboards(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 0);
+    showLeaderboards();
+    return 0;
+}
+
+/** Load Achivements
+*/
+static int showAchivements(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 0);
+    showAchivements();
+    return 0;
 }
 
 static const luaL_reg Module_methods[] =
 {
-    {"authenticate", authenticate},
+    {"login", login},
+    {"reportScore", reportScore},
+    {"showLeaderboards", showLeaderboards},
+    {"showLeaderboard", showLeaderboard},
+    {"showAchivements", showAchivements},
     {0, 0}
 };
 
--- gamecenter/src/gamecenter.mm
+++ gamecenter/src/gamecenter.mm
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+
 #include <dmsdk/sdk.h>
 #include <dmsdk/graphics/graphics_native.h>
 #include "gamecenter_private.h"
@@ -12,29 +31,42 @@
 #if defined(DM_PLATFORM_IOS) || defined(DM_PLATFORM_OSX)
 NSString *const PresentAuthenticationViewController = @"present_authentication_view_controller";
 
-@interface GameKitHelper : NSObject
+/*#if defined(DM_PLATFORM_IOS)
+UIViewController *controller = ((UIWindow*)dmGraphics::GetNativeiOSUIWindow()).rootViewController;
+#else
+NSViewController *controller = ((NSWindow*)dmGraphics::GetNativeOSXNSWindow()).contentViewController;
+#endif*/
+
+#if defined(DM_PLATFORM_IOS)
+@interface GameKitManager : UIViewController <GKGameCenterControllerDelegate>
 {
-	#if defined(DM_PLATFORM_IOS)
 	@private UIViewController *m_authenticationViewController;
-	#else
-	@private NSViewController *m_authenticationViewController;
-	#endif
 	@private NSError *m_lastError;
 }
-+ (instancetype)sharedGameKitHelper;
++ (instancetype)sharedGameKitManager;
 @end
 
-@implementation GameKitHelper
+#else
+@interface GameKitManager : NSViewController <GKGameCenterControllerDelegate>
+{
+	@private NSViewController *m_authenticationViewController;
+	@private NSError *m_lastError;
+}
++ (instancetype)sharedGameKitManager;
+@end
+#endif
+
+@implementation GameKitManager
 BOOL _enableGameCenter;
 
-+ (instancetype)sharedGameKitHelper
++ (instancetype)sharedGameKitManager
 {
-    static GameKitHelper *sharedGameKitHelper;
+    static GameKitManager *sharedGameKitManager;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        sharedGameKitHelper = [[GameKitHelper alloc] init];
+        sharedGameKitManager = [[GameKitManager alloc] init];
     });
-    return sharedGameKitHelper;
+    return sharedGameKitManager;
 }
 
 - (id)init
@@ -106,36 +138,29 @@ BOOL _enableGameCenter;
 - (void)showAuthenticationViewController
 {	
     #if defined(DM_PLATFORM_IOS)
-	UIViewController *controller = ((UIWindow*)dmGraphics::GetNativeiOSUIWindow()).rootViewController;
-	#else
-	NSViewController *controller = ((NSWindow*)dmGraphics::GetNativeOSXNSWindow()).contentViewController;
-	#endif
-    
     [controller presentViewController:
        m_authenticationViewController
                           animated:YES
                           completion:nil];
+                          
+    #else                    
+    [self presentViewControllerAsModalWindow:m_authenticationViewController];     
+    #endif              
 }
 
 - (void)setLastError:(NSError *)error
 {
 	m_lastError = [error copy];
 	if (m_lastError) {
-	    NSLog(@"GameKitHelper ERROR: %@",
+	    NSLog(@"GameKitManager ERROR: %@",
 	    [[m_lastError userInfo] description]);
 	}
 }
 
-- (void) authenticate
+- (void) login
 {
-	#if defined(DM_PLATFORM_IOS)
-	UIViewController *controller = ((UIWindow*)dmGraphics::GetNativeiOSUIWindow()).rootViewController;
-	#else
-	NSViewController *controller = ((NSWindow*)dmGraphics::GetNativeOSXNSWindow()).contentViewController;
-	#endif
-	
 	[[NSNotificationCenter defaultCenter]
-       addObserver:controller
+       addObserver:self
        selector:@selector(showAuthenticationViewController)
        name:PresentAuthenticationViewController
        object:nil];
@@ -143,21 +168,77 @@ BOOL _enableGameCenter;
     [self authenticateLocalPlayer];
 }
 
+- (int) reportScore:(NSString*)leaderboardId score:(int)score {
+	GKScore* highScore = [[GKScore alloc] initWithLeaderboardIdentifier:leaderboardId];
+	highScore.value = (int64_t)score;
+	[GKScore reportScores:@[highScore] withCompletionHandler:^(NSError *error) {
+        NSLog(@"Game Center : report score : %@", [[error userInfo] description]);
+    }];
+    return 0;
+}
+
+- (void)showLeaderboard:(NSString*)leaderboardId {
+	[self showLeaderboards:leaderboardId];
+}
+
+- (void)showLeaderboards {
+	[self showLeaderboards:nil];
+}
+
+
+- (void)showLeaderboards:(NSString*)leaderboardId {
+	GKGameCenterViewController* gameCenterController = [[GKGameCenterViewController alloc] init];
+    gameCenterController.viewState = GKGameCenterViewControllerStateLeaderboards;
+    
+    if(leaderboardId != nil) {
+    	gameCenterController.leaderboardIdentifier = leaderboardId;
+    }
+    gameCenterController.gameCenterDelegate = self;
+    [self presentViewControllerAsModalWindow:gameCenterController];
+}
+
+- (void)showAchivements {
+	GKGameCenterViewController* gameCenterController = [[GKGameCenterViewController alloc] init];
+    gameCenterController.viewState = GKGameCenterViewControllerStateAchievements;
+    gameCenterController.gameCenterDelegate = self;
+    [self presentViewControllerAsModalWindow:gameCenterController];   
+}
+
+- (void)gameCenterViewControllerDidFinish:(GKGameCenterViewController*) gameCenterViewController {
+    [self dismissViewController:gameCenterViewController];
+}
+
 - (void)dealloc
 {
-    #if defined(DM_PLATFORM_IOS)
-	UIViewController *controller = ((UIWindow*)dmGraphics::GetNativeiOSUIWindow()).rootViewController;
-	#else
-	NSViewController *controller = ((NSWindow*)dmGraphics::GetNativeOSXNSWindow()).contentViewController;
-	#endif
-    
-    [[NSNotificationCenter defaultCenter] removeObserver:controller];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 @end
 
-int authenticate() {
-	[[GameKitHelper sharedGameKitHelper] authenticate];
+// BEGIN API
+int login() {
+	[[GameKitManager sharedGameKitManager] login];
 	return 0;
 }
+
+int reportScore(const char *leaderboardId, int score) {
+	return [[GameKitManager sharedGameKitManager] reportScore:@(leaderboardId) score:score];
+}
+
+int showLeaderboards() {
+	[[GameKitManager sharedGameKitManager] showLeaderboards];
+	return 0;
+}
+
+int showLeaderboard(const char *leaderboardId) {
+	[[GameKitManager sharedGameKitManager] showLeaderboard:@(leaderboardId)];
+	return 0;
+}
+
+int showAchivements() {
+	[[GameKitManager sharedGameKitManager] showAchivements];
+	return 0;
+}
+// END API
+
 #endif // DM_PLATFORM_IOS/DM_PLATFORM_OSX
